52f97873021784eebe109898cb0bb295442bb14c
littleflute <13699175164@sina.cn>
2017-07-08 14:29:14 -0500

    Add files via upload

---
+++ Wilco/Being There Disc 1/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "02 Far, Far Away.mp3";		
+f[2]	= "04 Outtasite (Outta Mind).mp3";		
+f[3]	= "05 Forget the Flowers.mp3";		
+f[4]	= "06 Red-Eyed and Blue.mp3";		
+f[5]	= "07 I Got You (At the End of the Century).mp3";		
+f[6]	= "08 What's the World Got in Store.mp3";		
+f[7]	= "09 Hotel Arizona.mp3";		
+f[8]	= "10 Say You Miss Me.mp3";		
+f[9]	= "Misunderstood.mp3";		
+f[10]	= "Monday.mp3";		
---
+++ Wilco/Being There Disc 2/m.js
@@ -0,0 +1,11 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Sunken Treasure.mp3";		
+f[2]	= "02 Someday Soon.mp3";		
+f[3]	= "03 Outta Mind (Outta Sight).mp3";		
+f[4]	= "04 Someone Else's Song.mp3";		
+f[5]	= "06 (Was I) In Your Dreams.mp3";		
+f[6]	= "07 Why Would You Wanna Live.mp3";		
+f[7]	= "08 The Lonely 1.mp3";		
+f[8]	= "09 Dreamer in My Dreams.mp3";		
+f[9]	= "Kingpin.mp3";		
