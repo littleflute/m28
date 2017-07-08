ab3b15c2885d1a690c9713fa6a212f16d9ca6bc0
littleflute <13699175164@sina.cn>
2017-07-08 14:29:26 -0500

    Add files via upload

---
+++ Waylon Jennings/Country Legends [2005]/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Sally Was a Good Girl.mp3";		
+f[2]	= "04 It's So Easy.mp3";		
+f[3]	= "05 Love's Gonna Live Here Again.mp3";		
+f[4]	= "06 Don't Think Twice.mp3";		
+f[5]	= "07 Big Mammou.mp3";		
+f[6]	= "08 White Lightning.mp3";		
+f[7]	= "09 Burning Memories.mp3";		
+f[8]	= "10 Dream Baby.mp3";		
+f[9]	= "13 When Sin Stops.mp3";		
+f[10]	= "14 Jole Blon.mp3";		
+f[11]	= "15 Rhythm - Blues.mp3";		
+f[12]	= "Abilene.mp3";		
+f[13]	= "Crying.mp3";		
+f[14]	= "Lorena.mp3";		
+f[15]	= "Money.mp3";		
